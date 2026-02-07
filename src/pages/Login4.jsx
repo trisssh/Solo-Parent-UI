25beec59c87ef5751c2706c91a57f89eb8b6c5af
trisssh <samillanotrisha768@gmail.com>
2026-02-07 18:46:49 +0800

    `Refactored Login4.jsx page layout and styles`

--- src/pages/Login4.jsx
+++ src/pages/Login4.jsx
@@ -114,7 +114,7 @@ export default function Login4() {
       </div>
 
       {/* Right Section (Login Form) */}
-      <div className="flex flex-col justify-center items-center gap-6 mx-4">
+      <div className="flex flex-col justify-center items-center gap-6 mx-3">
         <div className="relative p-1 rounded-2xl overflow-hidden">
           {/* Animated Border */}
           <div
@@ -124,141 +124,146 @@ export default function Login4() {
           ></div>
 
           {/* Content */}
-          <div className="relative bg-white drop-shadow-[0_0_0.5rem_#FF2424] rounded-2xl p-2">
-            <div className="p-4 sm:p-8 md:p-10 rounded-2xl">
-              <div className="flex items-center justify-between">
-                {/* Logo Img Example */}
-                <div className="bg-white size-14 rounded-full flex justify-center items-center">
-                  <img src="spc.png" className="size-14" />
-                </div>
-                <div className="ml-2 md:ml-6 text-center">
-                  <h2 className="font-bold uppercase text-lg sm:text-2xl text-gray-800">
-                    Solo Parent System
-                  </h2>
-                  <p className="capitalize font-semibold text-xs md:text-right tracking-wide text-gray-600 hidden lg:block">
-                    City of San Juan, Metro Manila
-                  </p>
-                </div>
+          <div className="bg-white drop-shadow-[0_0_0.5rem_#FF2424]  shadow-lg rounded-xl w-full max-w-md p-6">
+            <div className="flex items-center justify-center mb-6">
+              {/* Logo Img Example */}
+              <div className="bg-white size-14 rounded-full flex justify-center items-center">
+                <img src="spc.png" className="size-14" />
               </div>
+              <div className="ml-2 md:ml-6 text-center">
+                <h2 className="font-bold uppercase text-base sm:text-xl text-gray-800">
+                  Solo Parent System
+                </h2>
+                <p className="capitalize font-semibold text-xs md:text-right tracking-wide text-gray-600">
+                  City of San Juan, Metro Manila
+                </p>
+              </div>
+            </div>
 
-              <div className="w-full max-w-md mt-4 md:mt-6">
-                <h2 className="text-xl md:text-2xl font-bold text-gray-800">
+            <div>
+              <article className="mb-6">
+                <h2 className="text-2xl font-bold text-gray-800 mb-1">
                   Log in to your Account
                 </h2>
+                <p className="font-semibold text-base text-gray-600 text-justify ">
+                  Please make sure you have a registered account before logging
+                  in.
+                </p>
+              </article>
 
-                {/* FORM START */}
-                <form onSubmit={handleLogin}>
-                  {/* Email */}
-                  <div className="relative w-full mt-4 rounded-lg">
-                    <input
-                      type="email"
-                      id="email"
-                      value={email}
-                      onChange={(e) => setEmail(e.target.value)}
-                      className="block px-3 pb-2.5 pt-3 w-full text-lg text-gray-800 font-semibold bg-transparent rounded appearance-none focus:outline-none focus:ring-0 border border-[#A6A6A6] focus:border-gray-500 peer shadow-md"
-                      placeholder=""
-                      name="email"
-                    />
-                    <label
-                      htmlFor="email"
-                      className="absolute text-2xl font-bold text-[var(--gray-2)] duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white px-2 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4 left-3"
-                    >
-                      Email
-                    </label>
-                  </div>
+              {/* FORM */}
+              <form onSubmit={handleLogin}>
+                {/* Email */}
+                <div className="relative w-full my-4 rounded-lg">
+                  <input
+                    type="email"
+                    id="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    className="block px-3 pb-2.5 pt-3 w-full text-lg text-gray-800 font-semibold bg-transparent rounded appearance-none focus:outline-none focus:ring-0 border border-[#A6A6A6] focus:border-gray-500 peer shadow-md"
+                    placeholder=""
+                    name="email"
+                  />
+                  <label
+                    htmlFor="email"
+                    className="absolute text-2xl font-bold text-[var(--gray-2)] duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white px-2 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4 left-3"
+                  >
+                    Email
+                  </label>
+                </div>
 
-                  {/* Password */}
-                  <div className="relative w-full mt-4 rounded-lg">
-                    <input
-                      type={showPassword ? "text" : "password"}
-                      id="password"
-                      value={password}
-                      onChange={(e) => setPassword(e.target.value)}
-                      className="block px-3 pb-2.5 pt-3 w-full text-lg text-gray-800 font-semibold bg-transparent rounded appearance-none focus:outline-none focus:ring-0 border border-[#A6A6A6] focus:border-gray-500 peer shadow-md pr-10"
-                      placeholder=" "
-                      name="password"
-                    />
-                    <label
-                      htmlFor="password"
-                      className="absolute text-2xl font-bold text-[var(--gray-2)] duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white px-2
+                {/* Password */}
+                <div className="relative w-full mt-4 rounded-lg">
+                  <input
+                    type={showPassword ? "text" : "password"}
+                    id="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    className="block px-3 pb-2.5 pt-3 w-full text-lg text-gray-800 font-semibold bg-transparent rounded appearance-none focus:outline-none focus:ring-0 border border-[#A6A6A6] focus:border-gray-500 peer shadow-md pr-10"
+                    placeholder=" "
+                    name="password"
+                  />
+                  <label
+                    htmlFor="password"
+                    className="absolute text-2xl font-bold text-[var(--gray-2)] duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white px-2
                       peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0
                       peer-focus:scale-75 peer-focus:-translate-y-4 left-3"
-                    >
-                      Password
-                    </label>
+                  >
+                    Password
+                  </label>
 
-                    {/* Eye icon */}
-                    <button
-                      type="button"
-                      onClick={() => setShowPassword(!showPassword)}
-                      className="absolute right-3 top-1/2 -translate-y-1/2 text-gray-500 hover:text-gray-700"
-                    >
-                      {showPassword ? (
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          strokeWidth={1.5}
-                          stroke="currentColor"
-                          className="size-6"
-                        >
-                          <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88"
-                          />
-                        </svg>
-                      ) : (
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          strokeWidth={1.5}
-                          stroke="currentColor"
-                          className="size-6"
-                        >
-                          <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z"
-                          />
-                          <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
-                          />
-                        </svg>
-                      )}
-                    </button>
-                  </div>
-                  {/* Forgot Password Link */}
-                  <div className="text-right">
-                    <Link
-                      to="/forgot-password"
-                      className="text-xs underline hover:text-red-600 text-red-500 cursor-pointer"
-                    >
-                      Forgot Password?
-                    </Link>
-                  </div>
-
-                  {/* Login Button */}
+                  {/* Eye icon */}
                   <button
-                    type="submit"
-                    className="text-base uppercase font-bold tracking-widest mt-6 w-full py-3 rounded bg-gradient-to-r from-[var(--red-2)] to-red-600 text-white font-semibold hover:from-[var(--red-2)] hover:to-red-700 transition-all duration-300 hover:scale-105 cursor-pointer shadow shadow-gray-700"
+                    type="button"
+                    onClick={() => setShowPassword(!showPassword)}
+                    className="absolute right-3 top-1/2 -translate-y-1/2 text-gray-500 hover:text-gray-700"
                   >
-                    Login
+                    {showPassword ? (
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="currentColor"
+                        className="size-6"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88"
+                        />
+                      </svg>
+                    ) : (
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="currentColor"
+                        className="size-6"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z"
+                        />
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                        />
+                      </svg>
+                    )}
                   </button>
-                </form>
-                {/* Footer */}
-                <div className="mt-2 text-center text-sm text-gray-600">
-                  Don’t have an account?
-                  <a
-                    href="#"
-                    className="text-[var(--red-2)] ml-1 font-bold hover:underline"
-                  >
-                    Create an account
-                  </a>
                 </div>
+                {/* Forgot Password Link */}
+                <div className="text-right">
+                  <Link
+                    to="/forgot-password"
+                    className="text-xs underline hover:text-red-600 text-red-500 cursor-pointer"
+                  >
+                    Forgot Password?
+                  </Link>
+                </div>
+
+                {/* Login Button */}
+                <button
+                  type="submit"
+                  className="text-base uppercase font-bold tracking-widest mt-6 w-full py-3 rounded bg-gradient-to-r from-[var(--red-2)] to-red-600 text-white font-semibold hover:from-[var(--red-2)] hover:to-red-700 transition-all duration-300 hover:scale-105 cursor-pointer shadow shadow-gray-700"
+                >
+                  Login
+                </button>
+              </form>
+
+              {/* Link */}
+              <div className="mt-2 text-center text-sm text-gray-600">
+                Don’t have an account?
+                <a
+                  href="#"
+                  className="text-[var(--red-2)] ml-1 font-bold hover:underline"
+                >
+                  Create an account
+                </a>
               </div>
             </div>
           </div>
