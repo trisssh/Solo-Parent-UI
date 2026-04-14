ecd92ab219abf38414f12ee4010d1fe73553bd7a
trisssh <samillanotrisha768@gmail.com>
2026-04-14 08:08:55 +0800

    test

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -87,6 +87,9 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+# Database
+# https://docs.djangoproject.com/en/6.0/ref/settings/#databases
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
