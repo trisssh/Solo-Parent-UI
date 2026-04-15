c3d4c82700c2ae918a119f1cb395516b1e09e321
trisssh <samillanotrisha768@gmail.com>
2026-04-15 14:58:21 +0800

    update id template with white bg remover for esign

--- frontend/src/dashboards/user/UserDashboard.jsx
+++ frontend/src/dashboards/user/UserDashboard.jsx
@@ -13,6 +13,7 @@ export default function UserDashboard() {
   const [showModal, setShowModal] =useState(false);
   const navigate = useNavigate();
   const [showBack, setShowBack] = useState(false);
+  const [cleanSignature, setCleanSignature] = useState(null);
 
   //View ID
   const toggleModal = () => {
@@ -134,6 +135,52 @@ useEffect(() => {
 
     return `${month} ${day}, ${year}`;
   };
+
+
+  //REMOVE WHITE BG
+  const removeWhiteBg = (imgUrl) => {
+  return new Promise((resolve) => {
+    const img = new Image();
+    img.crossOrigin = "Anonymous";
+    img.src = imgUrl;
+
+    img.onload = () => {
+      const canvas = document.createElement("canvas");
+      const ctx = canvas.getContext("2d");
+
+      canvas.width = img.width;
+      canvas.height = img.height;
+
+      ctx.drawImage(img, 0, 0);
+
+      const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+      const data = imageData.data;
+
+      for (let i = 0; i < data.length; i += 4) {
+        const r = data[i];
+        const g = data[i + 1];
+        const b = data[i + 2];
+
+        // detect white pixels
+        if (r > 240 && g > 240 && b > 240) {
+          data[i + 3] = 0; // make transparent
+        }
+      }
+
+      ctx.putImageData(imageData, 0, 0);
+
+      resolve(canvas.toDataURL("image/png"));
+    };
+  });
+};
+
+useEffect(() => {
+  if (signatureImageUrl) {
+    removeWhiteBg(signatureImageUrl).then((result) => {
+      setCleanSignature(result);
+    });
+  }
+}, [signatureImageUrl]);
 
   // console.log(userInfo?.image);
 
@@ -598,7 +645,7 @@ useEffect(() => {
                         {/* SIGNATURE */}
                         {signatureImageUrl && (
                           <img
-                            src={signatureImageUrl}
+                            src={cleanSignature || signatureImageUrl}
                             className="absolute bottom-[5px] left-[53px] w-[83px]"
                           />
                         )}
